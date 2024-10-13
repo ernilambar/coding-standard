6ff4a780c5c46671590bd4f1c62af97f533ba949
Nilambar Sharma <nilambar@outlook.com>
2024-10-13 22:20:51 +0545

    Settings fixes

--- NilambarCodingStandard/Sniffs/Security/SettingSanitizationSniff.php
+++ NilambarCodingStandard/Sniffs/Security/SettingSanitizationSniff.php
@@ -7,76 +7,64 @@
 
 namespace NilambarCodingStandard\Sniffs\Security;
 
-use PHP_CodeSniffer\Files\File;
-use PHP_CodeSniffer\Sniffs\Sniff;
-use PHP_CodeSniffer\Util\Tokens;
+use PHPCSUtils\Utils\PassedParameters;
+use WordPressCS\WordPress\AbstractFunctionParameterSniff;
 
 /**
  * Detect sanitization in register_setting().
  *
  * @since 1.0.0
  */
-final class SettingSanitizationSniff implements Sniff {
+final class SettingSanitizationSniff extends AbstractFunctionParameterSniff {
 
 	/**
-	 * Returns an array of tokens this test wants to listen for.
+	 * The group name for this group of functions.
 	 *
-	 * @return array
+	 * @since 1.0.0
+	 *
+	 * @var string
 	 */
-	public function register() {
-		return [ T_STRING ];
-	}
+	protected $group_name = 'register_setting';
 
 	/**
-	 * Processes this sniff, when one of its tokens is encountered.
+	 * List of functions to examine.
 	 *
-	 * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
-	 * @param int                         $stackPtr  The position of the current token
-	 *                                               in the stack passed in $tokens.
+	 * @since 1.0.0
+	 *
+	 * @var array<string, true> Key is function name, value irrelevant.
+	 */
+
+	protected $target_functions = [
+		'register_setting' => true,
+	];
+
+	/**
+	 * Process the parameters of a matched function.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int    $stackPtr        The position of the current token in the stack.
+	 * @param string $group_name      The name of the group which was matched.
+	 * @param string $matched_content The token content (function name) which was matched
+	 *                                in lowercase.
+	 * @param array  $parameters      Array with information about the parameters.
 	 *
 	 * @return void
 	 */
-	public function process( File $phpcsFile, $stackPtr ) {
-		$tokens = $phpcsFile->getTokens();
+	public function process_parameters( $stackPtr, $group_name, $matched_content, $parameters ) {
+		$third_param = PassedParameters::getParameterFromStack( $parameters, 3, 'args' );
 
-		// Check if the function call is to `register_setting`.
-		if ( 'register_setting' === $tokens[ $stackPtr ]['content'] ) {
-			$openParenthesis = $phpcsFile->findNext( Tokens::$emptyTokens, $stackPtr + 1, null, true, null, true );
+		if ( false === $third_param ) {
+			$error = 'Sanitization missing for register_setting().';
+			$this->phpcsFile->addError( $error, $stackPtr, 'Missing' );
+			return;
+		}
 
-			if ( T_OPEN_PARENTHESIS === $tokens[ $openParenthesis ]['code'] ) {
-				$closeParenthesis = $tokens[ $openParenthesis ]['parenthesis_closer'];
+		$third_token_type = $this->tokens[ $third_param['start'] + 1 ]['code'];
 
-				// Find the third parameter start.
-				$nextComma = $openParenthesis;
-				for ( $i = 0; $i < 2; $i++ ) {
-					$nextComma = $phpcsFile->findNext( T_COMMA, $nextComma + 1, $closeParenthesis );
-
-					// Less than three parameters.
-					if ( false === $nextComma ) {
-						$error = 'Sanitization missing for register_setting().';
-						$phpcsFile->addError( $error, $stackPtr, 'Missing' );
-						return;
-					}
-				}
-
-				// Move past whitespace to the third parameter.
-				$thirdParamStart = $phpcsFile->findNext( Tokens::$emptyTokens, $nextComma + 1, $closeParenthesis, true );
-
-				if ( false === $thirdParamStart ) {
-					// No third parameter.
-					$error = 'Sanitization missing for register_setting().';
-					$phpcsFile->addError( $error, $stackPtr, 'Missing' );
-					return;
-				}
-
-				// Check the third parameter.
-				$thirdTokenType = $tokens[ $thirdParamStart ]['code'];
-
-				if ( ! in_array( $thirdTokenType, [ T_CONSTANT_ENCAPSED_STRING, T_STRING, T_ARRAY, T_OPEN_SHORT_ARRAY, T_VARIABLE ], true ) ) {
-						$error = 'Invalid sanitization in third parameter of register_setting().';
-						$phpcsFile->addError( $error, $stackPtr, 'Invalid' );
-				}
-			}
+		if ( ! in_array( $third_token_type, [ T_CONSTANT_ENCAPSED_STRING, T_STRING, T_ARRAY, T_OPEN_SHORT_ARRAY, T_VARIABLE ], true ) ) {
+			$error = 'Invalid sanitization in third parameter of register_setting().';
+			$this->phpcsFile->addError( $error, $stackPtr, 'Invalid' );
 		}
 	}
 }
