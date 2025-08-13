b81524b77820d8680231da5a31f7a11da0b108e4
Nilambar Sharma <nilambar@outlook.com>
2025-08-13 14:38:04 +0545

    Update vars

--- NilambarCodingStandard/Vars/core-translations.php
+++ NilambarCodingStandard/Vars/core-translations.php
@@ -6,6 +6,15 @@
  */
 
 return [
+	'66 / 33',
 	'action label' . "\0" . 'Duplicate',
+	'block title' . "\0" . 'Date',
+	'block title' . "\0" . 'Content',
 	'Save Changes',
+	'Font collection "%s" not found.',
+	'font collection name'. "\0" .'Google Fonts',
+	'The following plugins must be activated first: %s.',
+	'Single item: %1$s (%2$s)',
+	'Audio title&hellip;',
+	'[%s] Your Site is Experiencing a Technical Issue',
 ];
