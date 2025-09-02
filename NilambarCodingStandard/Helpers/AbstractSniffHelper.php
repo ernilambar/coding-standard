64cd94562b1b47eb9b0ef5bf27b4ee30036609e3
Nilambar Sharma <nilambar@outlook.com>
2025-09-02 12:46:34 +0545

    Fix sniffer issues

--- NilambarCodingStandard/Helpers/AbstractSniffHelper.php
+++ NilambarCodingStandard/Helpers/AbstractSniffHelper.php
@@ -87,13 +87,15 @@ abstract class AbstractSniffHelper extends Sniff {
 	 * @return int|string The context (function/closure ID or 'global').
 	 */
 	public function get_context( $stackPtr ) {
-		if ( $context = $this->phpcsFile->getCondition( $stackPtr, \T_CLOSURE ) ) {
+		$context = $this->phpcsFile->getCondition( $stackPtr, \T_CLOSURE );
+		if ( $context ) {
 			return $context;
-		} elseif ( $context = $this->phpcsFile->getCondition( $stackPtr, \T_FUNCTION ) ) {
-			return $context;
-		} else {
-			return 'global';
 		}
+		$context = $this->phpcsFile->getCondition( $stackPtr, \T_FUNCTION );
+		if ( $context ) {
+			return $context;
+		}
+		return 'global';
 	}
 
 	/**
@@ -142,32 +144,32 @@ abstract class AbstractSniffHelper extends Sniff {
 		$out   = $this->tokens[ $stackPtr ]['content'];
 
 		while ( $limit > 0 ) {
-			// Find the next non-empty token
+			// Find the next non-empty token.
 			$nextToken = $this->phpcsFile->findNext( Tokens::$emptyTokens, $i, null, true, null, true );
 
 			if ( \T_OPEN_SQUARE_BRACKET === $this->tokens[ $nextToken ]['code'] ) {
-				// If it's an array, take everything between the brackets as part of the variable name
+				// If it's an array, take everything between the brackets as part of the variable name.
 				for ( $i = $nextToken; $i <= $this->tokens[ $nextToken ]['bracket_closer']; $i++ ) {
-					if ( ! in_array( $this->tokens[ $i ]['code'], Tokens::$emptyTokens ) ) {
+					if ( ! in_array( $this->tokens[ $i ]['code'], Tokens::$emptyTokens, true ) ) {
 						$out .= $this->tokens[ $i ]['content'];
 					}
 				}
-			} elseif ( $this->tokens[ $nextToken ]['code'] === \T_OBJECT_OPERATOR
-				|| $this->tokens[ $nextToken ]['code'] === \T_DOUBLE_COLON ) {
-				// If it's :: or -> then check if the following thing is a string..
+			} elseif ( \T_OBJECT_OPERATOR === $this->tokens[ $nextToken ]['code']
+				|| \T_DOUBLE_COLON === $this->tokens[ $nextToken ]['code'] ) {
+				// If it's :: or -> then check if the following thing is a string.
 				$objectThing = $this->phpcsFile->findNext( Tokens::$emptyTokens, $nextToken + 1, null, true, null, true );
 
-				// It could be a variable name or function name
-				if ( $this->tokens[ $objectThing ]['code'] === \T_STRING ) {
+				// It could be a variable name or function name.
+				if ( \T_STRING === $this->tokens[ $objectThing ]['code'] ) {
 					$lookAhead = $this->phpcsFile->findNext( Tokens::$emptyTokens, $objectThing + 1, null, true, null, true );
-					if ( $this->tokens[ $lookAhead ]['code'] === \T_OPEN_PARENTHESIS ) {
-						// It's a function name, so ignore it
+					if ( \T_OPEN_PARENTHESIS === $this->tokens[ $lookAhead ]['code'] ) {
+						// It's a function name, so ignore it.
 						break;
 					}
 					$out .= '->' . $this->tokens[ $objectThing ]['content'];
 					$i    = $objectThing + 1;
-				} elseif ( $this->tokens[ $objectThing ]['code'] === \T_LNUMBER ) {
-					// It's a numeric array index
+				} elseif ( \T_LNUMBER === $this->tokens[ $objectThing ]['code'] ) {
+					// It's a numeric array index.
 					$out .= '[' . $this->tokens[ $objectThing ]['content'] . ']';
 					$i    = $objectThing + 1;
 
@@ -175,10 +177,10 @@ abstract class AbstractSniffHelper extends Sniff {
 					++$i;
 				}
 			} elseif ( \T_CLOSE_SQUARE_BRACKET === $this->tokens[ $nextToken ]['code'] ) {
-				// It's a ] so see what's next
+				// It's a ] so see what's next.
 				++$i;
 			} else {
-				// Anything else is not part of a variable so stop here
+				// Anything else is not part of a variable so stop here.
 				break;
 			}
 
@@ -237,7 +239,7 @@ abstract class AbstractSniffHelper extends Sniff {
 			return true;
 		}
 
-		// Check if this is an array assignment, e.g., `$var['key'] = 'val';` .
+		// Check if this is an array assignment, e.g., `$var['key'] = 'val';`.
 		if ( \T_OPEN_SQUARE_BRACKET === $this->tokens[ $next_non_empty ]['code']
 			&& isset( $this->tokens[ $next_non_empty ]['bracket_closer'] )
 		) {
@@ -275,7 +277,7 @@ abstract class AbstractSniffHelper extends Sniff {
 			foreach ( array_reverse( $this->tokens[ $stackPtr ]['nested_parenthesis'], true ) as $start => $end ) {
 				if ( isset( $this->tokens[ $start ]['parenthesis_owner'] ) ) {
 					$ownerPtr = $this->tokens[ $start ]['parenthesis_owner'];
-					if ( in_array( $this->tokens[ $ownerPtr ]['code'], [ \T_IF, \T_ELSEIF ] ) ) {
+					if ( in_array( $this->tokens[ $ownerPtr ]['code'], [ \T_IF, \T_ELSEIF ], true ) ) {
 						return $ownerPtr;
 					}
 				}
@@ -309,19 +311,19 @@ abstract class AbstractSniffHelper extends Sniff {
 	 * @return array<int, int>|false Array with start and end positions or false.
 	 */
 	protected function get_scope_from_condition( $stackPtr ) {
-		if ( ! in_array( $this->tokens[ $stackPtr ]['code'], [ \T_IF, \T_ELSEIF, \T_ELSE ] ) ) {
+		if ( ! in_array( $this->tokens[ $stackPtr ]['code'], [ \T_IF, \T_ELSEIF, \T_ELSE ], true ) ) {
 			return false;
 		}
 		if ( isset( $this->tokens[ $stackPtr ]['scope_opener'] ) ) {
-			// if ( $foo ) { bar(); }
+			// Has a scope block with curly braces.
 			return [ $this->tokens[ $stackPtr ]['scope_opener'], $this->tokens[ $stackPtr ]['scope_closer'] ];
 		} elseif ( isset( $this->tokens[ $stackPtr ]['parenthesis_closer'] ) ) {
-			// if ( $foo ) bar();
+			// Has parentheses but no scope block.
 			$start = $this->next_non_empty( $this->tokens[ $stackPtr ]['parenthesis_closer'] + 1 );
 			$end   = $this->phpcsFile->findEndOfStatement( $start );
 			return [ $start, $end ];
 		} else {
-			// else foo();
+			// No parentheses or scope block.
 			$start = $this->next_non_empty( $stackPtr + 1 );
 			$end   = $this->phpcsFile->findEndOfStatement( $start );
 			return [ $start, $end ];
@@ -339,16 +341,16 @@ abstract class AbstractSniffHelper extends Sniff {
 	 */
 	protected function has_else( $stackPtr ) {
 		if ( isset( $this->tokens[ $stackPtr ]['scope_closer'] ) ) {
-			// it has a parenthesis block if () { foo(); }
+			// Has a scope block with curly braces.
 			$nextPtr = $this->next_non_empty( $this->tokens[ $stackPtr ]['scope_closer'] + 1 );
-			if ( $nextPtr && in_array( $this->tokens[ $nextPtr ]['code'], [ \T_ELSE, \T_ELSEIF ] ) ) {
+			if ( $nextPtr && in_array( $this->tokens[ $nextPtr ]['code'], [ \T_ELSE, \T_ELSEIF ], true ) ) {
 				return $nextPtr;
 			}
 		} else {
-			// no parenthesis block if () foo();
+			// No scope block, check after the statement.
 			$endPtr  = $this->phpcsFile->findEndOfStatement( $stackPtr );
 			$nextPtr = $this->next_non_empty( $endPtr + 1 );
-			if ( $nextPtr && in_array( $this->tokens[ $nextPtr ]['code'], [ \T_ELSE, \T_ELSEIF ] ) ) {
+			if ( $nextPtr && in_array( $this->tokens[ $nextPtr ]['code'], [ \T_ELSE, \T_ELSEIF ], true ) ) {
 				return $nextPtr;
 			}
 		}
@@ -398,12 +400,13 @@ abstract class AbstractSniffHelper extends Sniff {
 	 * @return int|false The end of function call position or false.
 	 */
 	protected function end_of_function_call( $stackPtr ) {
-		if ( ! in_array( $this->tokens[ $stackPtr ]['code'], Tokens::$functionNameTokens ) ) {
+		if ( ! in_array( $this->tokens[ $stackPtr ]['code'], Tokens::$functionNameTokens, true ) ) {
 			return false;
 		}
 
 		$function_params = PassedParameters::getParameters( $this->phpcsFile, $stackPtr );
-		if ( $param = end( $function_params ) ) {
+		$param           = end( $function_params );
+		if ( $param ) {
 			return $this->next_non_empty( $param['end'] + 1 );
 		}
 
@@ -528,13 +531,13 @@ abstract class AbstractSniffHelper extends Sniff {
 	 */
 	protected function get_interpolated_variables( $stackPtr ) {
 		// It must be an interpolated string.
-		if ( in_array( $this->tokens[ $stackPtr ]['code'], [ \T_DOUBLE_QUOTED_STRING, \T_HEREDOC ] ) ) {
+		if ( in_array( $this->tokens[ $stackPtr ]['code'], [ \T_DOUBLE_QUOTED_STRING, \T_HEREDOC ], true ) ) {
 			$out = [];
 			// Regex for complex variables in strings.
 			$regex = '/\$([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)(?:\[[^\]]+\])*(?:->[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*/';
 			if ( preg_match_all( $regex, $this->tokens[ $stackPtr ]['content'], $matches ) ) {
 				foreach ( $matches[0] as $var ) {
-					// Normalize variations like {$foo} and ${foo}
+					// Normalize variations with curly braces and dollar signs.
 					$out[] = '$' . trim( $var, '${}' );
 				}
 			}
@@ -554,13 +557,13 @@ abstract class AbstractSniffHelper extends Sniff {
 	 * @return bool Whether the token is a constant.
 	 */
 	protected function is_defined_constant( $stackPtr ) {
-		// It must be a string
+		// It must be a string.
 		$ok_tokens = [
 			\T_SELF,
 			\T_PARENT,
 			\T_STRING,
 		];
-		if ( ! in_array( $this->tokens[ $stackPtr ]['code'], $ok_tokens ) ) {
+		if ( ! in_array( $this->tokens[ $stackPtr ]['code'], $ok_tokens, true ) ) {
 			return false;
 		}
 
@@ -568,15 +571,15 @@ abstract class AbstractSniffHelper extends Sniff {
 
 		$nextToken = $this->next_non_empty( $stackPtr + 1 );
 		if ( \T_DOUBLE_COLON === $this->tokens[ $nextToken ]['code'] ) {
-			// It might be `self::MYCONST` or `Table::MYCONST`
+			// It might be a class constant like self::MYCONST or Table::MYCONST.
 			$nextToken = $this->next_non_empty( $nextToken + 1 );
 			if ( \T_STRING !== $this->tokens[ $nextToken ]['code'] ) {
-				// Must be `self::$myvar` or something else that we don't recognize
+				// Must be self::$myvar or something else that we don't recognize.
 				return false;
 			}
 		}
-		if ( in_array( $this->tokens[ $nextToken ]['code'], $this->function_tokens ) ) {
-			// It's followed by a paren or similar, so it's not a constant
+		if ( in_array( $this->tokens[ $nextToken ]['code'], $this->function_tokens, true ) ) {
+			// It's followed by a paren or similar, so it's not a constant.
 			return false;
 		}
 
@@ -605,10 +608,10 @@ abstract class AbstractSniffHelper extends Sniff {
 		$prev  = $stackPtr;
 		$next  = $this->next_non_empty( $stackPtr );
 		while ( $next ) {
-			if ( in_array( $this->tokens[ $next ]['code'], $stops ) ) {
+			if ( in_array( $this->tokens[ $next ]['code'], $stops, true ) ) {
 				return $prev;
 			}
-			// If we found nested parens, jump to the end
+			// If we found nested parens, jump to the end.
 			if ( \T_OPEN_PARENTHESIS === $this->tokens[ $next ]['code'] && isset( $this->tokens[ $next ]['parenthesis_closer'] ) ) {
 				$prev = $this->tokens[ $next ]['parenthesis_closer'];
 				$next = $prev + 1;
@@ -652,7 +655,7 @@ abstract class AbstractSniffHelper extends Sniff {
 	 * @param int|null $endPtr   The end position or null to auto-detect.
 	 * @return array<string> Array of variable names.
 	 */
-	function find_variables_in_expression( $stackPtr, $endPtr = null ) {
+	protected function find_variables_in_expression( $stackPtr, $endPtr = null ) {
 		$tokens_to_find = [
 			\T_VARIABLE             => \T_VARIABLE,
 			\T_DOUBLE_QUOTED_STRING => \T_DOUBLE_QUOTED_STRING,
@@ -667,11 +670,12 @@ abstract class AbstractSniffHelper extends Sniff {
 
 		$newPtr = $stackPtr;
 		do {
-			if ( in_array( $this->tokens[ $newPtr ]['code'], [ \T_DOUBLE_QUOTED_STRING, \T_HEREDOC ] ) ) {
+			if ( in_array( $this->tokens[ $newPtr ]['code'], [ \T_DOUBLE_QUOTED_STRING, \T_HEREDOC ], true ) ) {
 				$out = array_merge( $out, $this->get_interpolated_variables( $newPtr ) );
 			} elseif ( \T_VARIABLE === $this->tokens[ $newPtr ]['code'] ) {
 				$out[] = $this->get_variable_as_string( $newPtr );
 			}
+		// @codingStandardsIgnoreLine
 		} while ( $newPtr = $this->phpcsFile->findNext( $tokens_to_find, $newPtr + 1, $endPtr, false, null, true ) );
 
 		return $out;
@@ -687,10 +691,11 @@ abstract class AbstractSniffHelper extends Sniff {
 	 * @param int|null $endPtr   The end position or null to auto-detect.
 	 * @return array<string> Array of function names.
 	 */
-	function find_functions_in_expression( $stackPtr, $endPtr = null ) {
+	protected function find_functions_in_expression( $stackPtr, $endPtr = null ) {
 		$out = [];
 
 		$newPtr = $stackPtr;
+		// phpcs:ignore Generic.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition
 		while ( $newPtr = $this->phpcsFile->findNext( [ \T_STRING ], $newPtr, $endPtr, false, null, true ) ) {
 			$lookahead = $this->next_non_empty( $newPtr + 1 );
 			if ( $lookahead && ( is_null( $endPtr ) || $lookahead <= $endPtr ) ) {
@@ -718,7 +723,7 @@ abstract class AbstractSniffHelper extends Sniff {
 			return false;
 		}
 
-		// We'll check all lines related to this function call, because placement can differ depending on exactly where we trigger in a multi-line expression
+		// We'll check all lines related to this function call, because placement can differ depending on exactly where we trigger in a multi-line expression.
 		$end = $this->end_of_function_call( $stackPtr );
 		if ( $end < $stackPtr ) {
 			$end = $stackPtr;
@@ -769,7 +774,7 @@ abstract class AbstractSniffHelper extends Sniff {
 			return str_replace( '`', '', $maybe[1] );
 		}
 
-		// SHOW TABLE STATUS and SHOW TABLES WHERE Name = 'wp_posts'
+		// SHOW TABLE STATUS and SHOW TABLES WHERE Name = 'wp_posts'.
 		if ( preg_match( '/^\s*SHOW\s+(?:TABLE\s+STATUS|(?:FULL\s+)?TABLES).+WHERE\s+Name\s*=\s*("|\')((?:[0-9a-zA-Z$_.-]|[\xC2-\xDF][\x80-\xBF])+)\\1/is', $query, $maybe ) ) {
 			return $maybe[2];
 		}
@@ -857,7 +862,7 @@ abstract class AbstractSniffHelper extends Sniff {
 			 * `foo( $bar ) ? $baz : ''` -> ternary expression
 			 * `foo( $bar ? $baz : '' )` -> not a ternary expression
 			 */
-			if ( in_array( $this->tokens[ $next ]['code'], Tokens::$functionNameTokens ) ) {
+			if ( in_array( $this->tokens[ $next ]['code'], Tokens::$functionNameTokens, true ) ) {
 				$next = $this->next_non_empty( $next + 1 );
 				if ( \T_OPEN_PARENTHESIS === $this->tokens[ $next ]['code'] && isset( $this->tokens[ $next ]['parenthesis_closer'] ) ) {
 					$next = $this->tokens[ $next ]['parenthesis_closer'];
@@ -865,7 +870,7 @@ abstract class AbstractSniffHelper extends Sniff {
 			}
 
 			if ( \T_INLINE_THEN === $this->tokens[ $next ]['code'] ) {
-				// Found a ternary; check the $allow_empty condition
+				// Found a ternary; check the $allow_empty condition.
 				if ( ! $allow_empty ) {
 					$lookahead = $this->next_non_empty( $next + 1 );
 					if ( \T_INLINE_ELSE === $this->tokens[ $lookahead ]['code'] ) {
--- NilambarCodingStandard/Sniffs/Security/VerifyNonceSniff.php
+++ NilambarCodingStandard/Sniffs/Security/VerifyNonceSniff.php
@@ -44,7 +44,8 @@ final class VerifyNonceSniff extends AbstractSniffHelper {
 					'wp_send_json_error',
 					'wp_nonce_ays',
 					'return',
-				]
+				],
+				true
 			) ) {
 				return $stackPtr;
 			}
@@ -77,64 +78,65 @@ final class VerifyNonceSniff extends AbstractSniffHelper {
 	 */
 	public function process_token( $stackPtr ) {
 		if ( 'wp_verify_nonce' === $this->tokens[ $stackPtr ]['content'] ) {
-			if ( $ifPtr = $this->is_conditional_expression( $stackPtr ) ) {
-				// We're in a conditional, something like if ( wp_verify_nonce() )
+			$ifPtr = $this->is_conditional_expression( $stackPtr );
+			if ( $ifPtr ) {
+				// We're in a conditional, something like if ( wp_verify_nonce() ).
 
 				if ( $this->expression_is_negated( $stackPtr ) ) {
-					// if ( !wp_verify_nonce() )
+					// If !wp_verify_nonce() condition.
 
 					list( $expression_start, $expression_end ) = $this->get_expression_from_condition( $ifPtr );
 					list( $scope_start, $scope_end )           = $this->get_scope_from_condition( $ifPtr );
 
-					// if ( $something && ! wp_verify_nonce( ... ) )
+					// if ( $something && ! wp_verify_nonce( ... ) ).
 					if ( $this->expression_contains_and( $expression_start, $expression_end ) && $this->scope_contains_error_terminator( $scope_start, $scope_end ) ) {
 						$andPtr = $this->expression_contains_and( $expression_start, $expression_end );
 						if ( $andPtr < $stackPtr ) {
-							// if ( ..something.. && ! wp_verify_nonce() ... )
+							// if ( ..something.. && ! wp_verify_nonce() ... ).
 							$operand_functions = array_count_values( $this->find_functions_in_expression( $expression_start, $andPtr ) );
-							// if ( ... wp_verify_nonce() && ! wp_verify_nonce() ... )
+							// if ( ... wp_verify_nonce() && ! wp_verify_nonce() ... ).
 							if ( isset( $operand_functions['wp_verify_nonce'] ) ) {
 								// This is ok, and we will have already checked the previous wp_verify_nonce(), so skip.
 								return;
 							}
 						} else {
-							// if ( ... !wp_verify_nonce() && ..something.. )
-							// This is ok since the nonce call comes before the &&
+							// if ( ... !wp_verify_nonce() && ..something.. ).
+							// This is ok since the nonce call comes before the &&.
 							return;
 						}
 						$this->phpcsFile->addError(
 							'Unsafe use of wp_verify_nonce() in expression %s.',
 							$stackPtr,
 							'UnsafeVerifyNonceNegatedAnd',
-							[ $this->tokens_as_string( $expression_start, $expression_end ) ], // [ $unsafe_expression, $method, $methodParam[ 'clean' ], rtrim( "\n" . join( "\n", $extra_context ) ) ],
+							[ $this->tokens_as_string( $expression_start, $expression_end ) ], // [ $unsafe_expression, $method, $methodParam[ 'clean' ], rtrim( "\n" . join( "\n", $extra_context ) ) ].
 							0,
 							false
 						);
 
 					}
 				} else {
-					// if ( wp_verify_nonce() )
-					// In this case we want the else {} part
+					// if ( wp_verify_nonce() ).
+					// In this case we want the else {} part.
 					$elsePtr = $this->has_else( $ifPtr );
 					if ( $elsePtr ) {
 						list( $expression_start, $expression_end ) = $this->get_expression_from_condition( $ifPtr );
 						list( $scope_start, $scope_end )           = $this->get_scope_from_condition( $elsePtr );
 
-						// if ( $something || wp_verify_nonce( ... ) )
+						// if ( $something || wp_verify_nonce( ... ) ).
 						if ( $this->expression_contains_or( $expression_start, $expression_end ) && $this->scope_contains_error_terminator( $scope_start, $scope_end ) ) {
 
 							$orPtr = $this->expression_contains_or( $expression_start, $expression_end );
 							if ( $orPtr < $stackPtr ) {
-								// if ( ..something.. || wp_verify_nonce() ... )
+								// if ( ..something.. || wp_verify_nonce() ... ).
 								$operand_functions = array_count_values( $this->find_functions_in_expression( $expression_start, $orPtr ) );
-								// If the previous "something" was another wp_verify_nonce() call then we're fine, ignore
+								// If the previous "something" was another wp_verify_nonce() call then we're fine, ignore.
 								if ( isset( $operand_functions['wp_verify_nonce'] ) ) {
 									return;
 								}
 							} else {
-								// if ( wp_verify_nonce || ..something.. )
+								// if ( wp_verify_nonce || ..something.. ).
 								$operand_functions = array_count_values( $this->find_functions_in_expression( $orPtr, $expression_end ) );
-								// If the next "something" is another wp_verify_nonce() call then we're fine, ignore
+								// If the next "something" is another wp_verify_nonce() call then we're fine, ignore.
 								if ( isset( $operand_functions['wp_verify_nonce'] ) ) {
 									return;
 								}
@@ -143,7 +145,7 @@ final class VerifyNonceSniff extends AbstractSniffHelper {
 								'Possibly unsafe use of wp_verify_nonce() in expression %s.',
 								$stackPtr,
 								'UnsafeVerifyNonceElse',
-								[ $this->tokens_as_string( $expression_start, $expression_end ) ], // [ $unsafe_expression, $method, $methodParam[ 'clean' ], rtrim( "\n" . join( "\n", $extra_context ) ) ],
+								[ $this->tokens_as_string( $expression_start, $expression_end ) ], // [ $unsafe_expression, $method, $methodParam[ 'clean' ], rtrim( "\n" . join( "\n", $extra_context ) ) ].
 								0,
 								false
 							);
@@ -152,7 +154,7 @@ final class VerifyNonceSniff extends AbstractSniffHelper {
 				}
 			} elseif ( ! $this->is_return_statement( $stackPtr ) && ! $this->is_assignment_statement( $stackPtr ) ) {
 
-					// wp_verify_nonce() used as an unconditional statement - most likely mistaken for check_admin_referer()
+					// wp_verify_nonce() used as an unconditional statement - most likely mistaken for check_admin_referer().
 					$this->phpcsFile->addError(
 						'Unconditional call to wp_verify_nonce(). Consider using check_admin_referer() instead.',
 						$stackPtr,
